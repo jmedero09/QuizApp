36ec46e3e83b474af0dc15ace157353b861cb703
Jesus Medero <jmedero09@gmail.com>
2016-07-21 18:48:40 -0400

    all fucntions working moving to new game fuction

--- js/script.js
+++ js/script.js
@@ -1,70 +1,104 @@
 $(document).ready(function(){
 
 'use strict'
-var questions=[
+var questions = [
 	
 	{
 		question:'Who did Rocky fight in Rocky 3',
-		choices:[' Apollo Creed',' Clubber Lang',' Ivan Drago',' Tommy Gunn'],
-		answer:' Clubber Lang'
+		choices:['Apollo Creed','Clubber Lang','Ivan Drago','Tommy Gunn'],
+		answer:'Clubber Lang'
 	},
 	{
 		question:'Which Holiday did Rocky and Adrian have their first date',
-		choices:[' Valentines Day',' Easter',' Christmas',' Thankgsgiving'],
-		answer:' Thankgsgiving'
+		choices:['Valentines Day','Easter','Christmas','Thankgsgiving'],
+		answer:'Thankgsgiving'
 	},
 	{
 		question:'What is the name of the gym that Rocky trains at',
-		choices:[' Gotham Gym',' Church Street Gym',' Mighty Micks',' The Wild Card'],
-		answer:' Mighty Micks'
+		choices:['Gotham Gym','Church Street Gym','Mighty Micks','The Wild Card'],
+		answer:'Mighty Micks'
 	},
 	{
 		question:'What is the name  of Rockys Dog',
-		choices:[' Butkus',' Cuff',' Link',' Moby Dick'],
-		answer:' Butkus'
+		choices:['Butkus','Cuff','Link','Moby Dick'],
+		answer:'Butkus'
 	},
 	{
 		question:'Who Played Apollo Creed',
-		choices:[' Charles Woodson',' Carl Weathers',' Michael B. Jordan',' Anthony Mackie'],
-		answer:' Carl Weathers'
+		choices:['Charles Woodson','Carl Weathers','Michael B. Jordan','Anthony Mackie'],
+		answer:'Carl Weathers'
 	}						
 
-]
+];
+
 var currentQuestion = 0;
 
+var questionNumber = 1;
+
+var correctAnswer = 0;
+
 function display(){
-			
-	$('p').append(questions[currentQuestion].question);
+	if(currentQuestion < questions.length){
+		$('p').append(questions[currentQuestion].question);
 
-	//for(var i = 0; i<questions[currentQuestion].choices.length;i++){
+		$('span').text(questionNumber+" of "+questions.length);
 
-		$('ul').append('<li>'+'<input type="radio" name="question1" value="1" id="quest1">'+
-	    '<label for="question1">'+questions[currentQuestion].choices[0]+'</label></li>');
+		for(var i = 0; i < questions[currentQuestion].choices.length; i++){
 
-		$('ul').append('<li>'+'<input type="radio" name="question1" value="1" id="quest2">'+
-	    '<label for="question1">'+questions[currentQuestion].choices[1]+'</label></li>');
+		// To dynamically set up the value attribut im concatinating 
+		$('ul').append('<li><input type="radio" name="question" value="' + questions[currentQuestion].choices[i] + '"/>' +
+			 questions[currentQuestion].choices[i] + '</li>');
+		}		
+	}
+	else{
+		$('p').append("You Got "+correctAnswer+"out of "+questions.length);
+	}
 
-	  	$('ul').append('<li>'+'<input type="radio" name="question1" value="1" id="quest3">'+
-	    '<label for="question1">'+questions[currentQuestion].choices[2]+'</label></li>');
-
-	    $('ul').append('<li>'+'<input type="radio" name="question1" value="1" id="quest4">'+
-	    '<label for="question1">'+questions[currentQuestion].choices[3]+'</label></li>');
-
-	//}
 }
- $( "input[name='submit']" ).on('click',function(){
- 	currentQuestion++;
- 	console.log(currentQuestion);
- 	$('ul').empty();
+
+function validateInput(input){
+	if(currentQuestion > questions.length){
+		currentQuestion = currentQuestion;
+		questionNumber = questionNumber;
+	}
+	else if(input === undefined){
+		console.log("dkjdskfjhksjdfhdksj")
+		currentQuestion = currentQuestion;
+		questionNumber = questionNumber;
+	}
+	else if(input === questions[currentQuestion].answer){
+
+console.log("nigga we made it");
+		currentQuestion++;
+		questionNumber++;
+		correctAnswer++;
+	}
+	else{
+
+console.log("awe man");
+	
+		currentQuestion++;
+		questionNumber++;
+		
+
+	}
+}
+ 
+$( "input[name='submit']" ).on('click', function() {
+ 	
+ 	validateInput($('input[type="radio"]:checked:first').val());
+
+console.log($('input[type="radio"]:checked:first').val());
+	
+	$('ul').empty();
+
  	$('p').empty();	
+
  	display();
- 	//alert($("input[name=question1]:checked").val());
- 	//questions[currentQuestion].choices[0];
  });
 
-//$('label').append(questions[0].choices[2]);
-//console.log(questions[0].choices[2]);
-display();
+ 	display();
+
 
 
 
